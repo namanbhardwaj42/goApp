1837d0f3f60721c24bfd372df274a1636c0806ec
naman <naman.bhardwaj42@gmail.com>
2023-02-16 14:39:20 +0530

    test page 2

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1> Welcome to <font color = 'red'>Hyenos</font></h1>")
+	fmt.Fprint(w, "<h1> Welcome to <font color = 'red'>Hyenos!</font></h1>")
 }
 func main() {
 	http.HandleFunc("/", handlerFunc)
