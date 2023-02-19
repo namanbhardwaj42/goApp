ea69e897ccd5a05baeae662a588add81a81d67e0
naman <naman.bhardwaj42@gmail.com>
2023-02-19 19:27:31 +0530

    2nd printline added

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Logo 2")
 	fmt.Fprint(w, "<h1> Welcome to <font color = 'red'>Hyenos!</font></h1>")
 }
 func main() {
