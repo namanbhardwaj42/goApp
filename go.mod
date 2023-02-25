e8a98fb1ee0c605ae79e7d389c8b2862de12eda6
naman <naman.bhardwaj42@gmail.com>
2023-02-26 00:56:27 +0530

    gorilla mux removed and julienschmidt router added

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module main
 
 go 1.19
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/julienschmidt/httprouter v1.3.0
+)
--- main.go
+++ main.go
@@ -4,23 +4,35 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/gorilla/mux"
+	"github.com/julienschmidt/httprouter"
 )
 
-func Home(w http.ResponseWriter, r *http.Request) {
+func Home(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>Welcome to <font color = 'orange'>Firenos</font></h1>")
 }
 
-func Contact(w http.ResponseWriter, r *http.Request) {
+func Contact(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "Hi! To get in touch please send an email to <a href = \"mailto:support@lenslocked.com\">support@lenslocked.com</a>")
 }
 
+func Faq(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Faq Page</h1><p>Under Development</p>")
+}
+
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>404 Page Not found!</h1>")
+}
+
 func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/", Home)
-	r.HandleFunc("/contact", Contact)
-	//http.HandleFunc("/", handlerFunc)
+	r := httprouter.New()
+	r.GET("/", Home)
+	r.GET("/contact", Contact)
+	r.GET("/faq", Faq)
+	r.NotFound = http.HandlerFunc(notFound)
 	http.ListenAndServe(":3000", r)
 }
