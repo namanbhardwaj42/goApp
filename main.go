e45a9048b313dc4ce907b3f48aa26f24cbaf8bae
naman <naman.bhardwaj42@gmail.com>
2023-02-25 14:10:24 +0530

    Mux | ServeMux Added

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	if r.URL.Path == "/" {
 		fmt.Fprint(w, "<h1>Welcome to <font color = 'red'>Firenos!</font></h1>")
-	} else if r.URL.Path == "/contact" {
+	} else if r.URL.Path == "/contact" || r.URL.Path == "/contact/" {
 		fmt.Fprint(w, "To get in touch please send an email to <a href = \"mailto:support@lenslocked.com\">support@lenslocked.com</a>")
 	} else {
 		w.WriteHeader(http.StatusNotFound)
@@ -17,6 +17,8 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	}
 }
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	mux := &http.ServeMux{}
+	mux.HandleFunc("/", handlerFunc)
+	//http.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":3000", mux)
 }
