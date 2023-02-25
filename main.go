39e2a8c32658fa8e0a602e00ab6d4e83690eb25a
naman <naman.bhardwaj42@gmail.com>
2023-02-25 13:14:34 +0530

    added two page content

--- main.go
+++ main.go
@@ -6,9 +6,14 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Logo 3/n")
-	fmt.Fprint(w, "<h1> Welcome to <font color = 'red'>Hyenos!</font></h1>")
+	w.Header().Set("Content-Type", "text/html")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Welcome to <font color = 'red'>Firenos!</font></h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "To get in touch please send an email to <a href = \"mailto:support@lenslocked.com\">support@lenslocked.com</a>")
+	}
 }
+
 func main() {
 	http.HandleFunc("/", handlerFunc)
 	http.ListenAndServe(":3000", nil)
