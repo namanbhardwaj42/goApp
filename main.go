36dfccd7b637a99efe3cd63887aa9662f1e2d221
naman <naman.bhardwaj42@gmail.com>
2023-02-28 19:34:49 +0530

    added new stuffs in template view

--- main.go
+++ main.go
@@ -2,27 +2,31 @@ package main
 
 import (
 	"fmt"
-	"html/template"
+	"main/views"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
 var (
-	homeTemplate    *template.Template
-	contactTemplate *template.Template
+	//homeTemplate    *template.Template
+	//contactTemplate *template.Template
+	homeView    *views.View
+	contactView *views.View
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if err := homeTemplate.Execute(w, nil); err != nil {
+	err := homeView.Template.ExecuteTemplate(w, homeView.Layout, nil)
+	if err != nil {
 		panic(err)
 	}
 }
 
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if err := contactTemplate.Execute(w, nil); err != nil {
+	err := contactView.Template.ExecuteTemplate(w, contactView.Layout, nil)
+	if err != nil {
 		panic(err)
 	}
 }
@@ -33,21 +37,26 @@ func Faq(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	var err error
-	homeTemplate, err = template.ParseFiles(
-		"views/home.gohtml",
-		"views/layouts/footer.gohtml",
-	)
-	if err != nil {
-		panic(err)
-	}
-	contactTemplate, err = template.ParseFiles(
-		"views/contact.gohtml",
-		"views/layouts/footer.gohtml",
-	)
-	if err != nil {
-		panic(err)
-	}
+	/*
+		var err error
+		homeTemplate, err = template.ParseFiles(
+			"views/home.gohtml",
+			"views/layouts/footer.gohtml",
+		)
+		if err != nil {
+			panic(err)
+		}
+		contactTemplate, err = template.ParseFiles(
+			"views/contact.gohtml",
+			"views/layouts/footer.gohtml",
+		)
+		if err != nil {
+			panic(err)
+		}
+	*/
+
+	homeView = views.NewView("bootstrap", "views/home.gohtml")
+	contactView = views.NewView("bootstrap", "views/contact.gohtml")
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
